71582592f73f8741082f0b9b594dc65a26183b77
Jayanta Biswas <bjayanta.me@gmail.com>
2019-02-12 17:54:53 +0600

    add readme, sql and update local env

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
-# Prototype
-Simple laravel 5.7 project for developer.
+# Office App
+Simple laravel 5.7 project for office management.
 
-## Features :
-- Laravel Collective 
-- Customize the model with model directory
-- Multi authentication system
-  - User panel
-  - Admin panel
-- Laravel passport
-- Localization
-  - English
-  - Bangla 
-- Multi Database Handle
-  - MySQL & Ready configuration
-    - How to Migrate and Rollback
-    - How to use in Model
-    - How to use in Query builder
+## Features:
+- Todo
+- 
--- public/doc/database/office.sql
+++ public/doc/database/office.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 09, 2019 at 11:42 AM
+-- Generation Time: Feb 12, 2019 at 12:51 PM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `prototype`
+-- Database: `office`
 --
 
 -- --------------------------------------------------------
@@ -53,15 +53,6 @@ CREATE TABLE `admins` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `admins`
---
-
-INSERT INTO `admins` (`id`, `name`, `phone`, `email`, `password`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Jayanta Biswas', '01903402828', 'bjayanta.neo@gmail.com', '$2y$10$/z7b8pAxERc9Jg0MD506xu4qWXW3tvzgt9FAjZ5k6SE3mOardojnC', 1, 'caouEsoTBs4sMneGEIn5eRsJTMEuujfn0rhxhGQquXGvFM2fpcdkWYqvbB4U', '2018-12-11 21:36:17', '2018-12-12 01:19:54'),
-(2, 'USITSolution', '01775219457', 'hello@usitsolution.net', '$2y$10$/z7b8pAxERc9Jg0MD506xu4qWXW3tvzgt9FAjZ5k6SE3mOardojnC', 1, 'ivgrOigAPIh4iV9HkwhaVXGINMuDthgSgQjyN67NHo1rlIqddVCEQBLiQzmm', '2018-12-11 21:36:17', '2018-12-12 01:20:01'),
-(3, 'Shibbir Ahmed', '01766263681', 'shibbirweb@gmail.com', '$2y$10$2j6.fes7CxsAx3/gnHMHKe/Rsgm6RN28mu4zOIgb.MGcn96NTfUOm', 1, NULL, '2019-01-04 15:17:02', '2019-01-04 15:17:02');
-
 -- --------------------------------------------------------
 
 --
@@ -72,15 +63,6 @@ CREATE TABLE `admin_role` (
   `admin_id` int(10) UNSIGNED NOT NULL,
   `role_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `admin_role`
---
-
-INSERT INTO `admin_role` (`admin_id`, `role_id`) VALUES
-(1, 1),
-(2, 3),
-(3, 1);
 
 -- --------------------------------------------------------
 
@@ -101,15 +83,16 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2018_12_01_102758_create_admins_table', 2),
-(4, '2018_12_03_124721_create_permissions_table', 3),
-(5, '2018_12_04_123752_create_roles_table', 4),
-(6, '2016_06_01_000001_create_oauth_auth_codes_table', 5),
-(7, '2016_06_01_000002_create_oauth_access_tokens_table', 5),
-(8, '2016_06_01_000003_create_oauth_refresh_tokens_table', 5),
-(9, '2016_06_01_000004_create_oauth_clients_table', 5),
-(10, '2016_06_01_000005_create_oauth_personal_access_clients_table', 5),
-(12, '2019_02_09_100842_create_metadata_table', 6);
+(3, '2016_06_01_000001_create_oauth_auth_codes_table', 1),
+(4, '2016_06_01_000002_create_oauth_access_tokens_table', 1),
+(5, '2016_06_01_000003_create_oauth_refresh_tokens_table', 1),
+(6, '2016_06_01_000004_create_oauth_clients_table', 1),
+(7, '2016_06_01_000005_create_oauth_personal_access_clients_table', 1),
+(8, '2018_12_01_102758_create_admins_table', 1),
+(9, '2018_12_03_124721_create_permissions_table', 1),
+(10, '2018_12_04_123752_create_roles_table', 1),
+(11, '2019_02_12_073945_create_sponsors_table', 1),
+(12, '2019_02_12_103133_create_todos_table', 2);
 
 -- --------------------------------------------------------
 
@@ -134,17 +117,8 @@ CREATE TABLE `oauth_access_tokens` (
 --
 
 INSERT INTO `oauth_access_tokens` (`id`, `user_id`, `client_id`, `name`, `scopes`, `revoked`, `created_at`, `updated_at`, `expires_at`) VALUES
-('0115bacdce11650939f8aa8d7396d24797ba47b8083890e31cc9e4a49104012304b10e1526db5584', 9, 3, 'MyApp', '[]', 0, '2019-02-05 06:04:29', '2019-02-05 06:04:29', '2020-02-05 12:04:29'),
-('0c2a65e0efd239c0028272d6424ba0dc195beef3575ac603253681c64b5e264abcb9f6ca767ca854', 6, 3, 'MyApp', '[]', 0, '2019-02-05 05:46:09', '2019-02-05 05:46:09', '2020-02-05 11:46:09'),
-('1fd6e95bf895de153f0f3d0fea10c6bd0b9b1608c7cc7d947acdc523baa5f9e6fc3aee5655e1112e', 1, 3, 'MyApp', '[]', 0, '2019-02-05 21:31:07', '2019-02-05 21:31:07', '2020-02-06 03:31:07'),
-('535d45f387cc266aecce0178c434df2624d797ae69d8882a033cdb148e323e3793ad7f38a6bee43d', 1, 3, 'MyApp', '[]', 0, '2019-02-05 04:56:14', '2019-02-05 04:56:14', '2020-02-05 10:56:14'),
-('623dea669927c5d28fa70802941774e14431efe79bab2417e83a02250630edf1308b51cee224a4c1', 1, 3, 'Hello', '[]', 1, '2019-02-04 22:41:53', '2019-02-04 22:41:53', '2020-02-05 04:41:53'),
-('839e8778108add8700b51e3a74cbe7c6900a5e13d7b445d256bb1dac5df4444dfc8398f31a975432', 9, 3, 'MyApp', '[]', 0, '2019-02-05 06:01:10', '2019-02-05 06:01:10', '2020-02-05 12:01:10'),
-('89b929ee63a5056170b41536ca82cc179a1278e09c15566932f6c4731eda653c1c71150e58cd1802', 5, 3, 'MyApp', '[]', 0, '2019-02-05 05:30:51', '2019-02-05 05:30:51', '2020-02-05 11:30:51'),
-('8d043baec7c74fe019d0e2bf4ccc8b381711adca835a31946ca432b8617ebc61181ccc2377068c43', 2, 1, 'MyApp', '[]', 0, '2019-02-04 13:24:55', '2019-02-04 13:24:55', '2020-02-04 19:24:55'),
-('8ef8b176bc542dca7925d3410e20bafac219eba297e1233482eeee98e28fd4322eba5fe529c2ff75', 3, 3, 'MyApp', '[]', 0, '2019-02-05 05:25:39', '2019-02-05 05:25:39', '2020-02-05 11:25:39'),
-('f30fc259dd5fc8e3f8364b37de3701ad2c26a30a358dcb1f44a61ed328a5eeeb178d704cfb6c6270', 2, 1, 'Hello', '[]', 0, '2019-02-04 13:19:03', '2019-02-04 13:19:03', '2020-02-04 19:19:03'),
-('f8ea74596e49ce241f61abe49a2d2db96d8cd3ae75901d5babcc21c27987a5813d13ad8cf1ef81ec', 1, 3, 'MyApp', '[]', 0, '2019-02-05 21:08:51', '2019-02-05 21:08:51', '2020-02-06 03:08:51');
+('02fb6bf5cd46dfea60f2e9b2d6467f97ed8075011eae09af78af85a90fcbf75800d08bfff3b7f2f7', 1, 1, 'Test', '[]', 1, '2019-02-12 03:25:35', '2019-02-12 03:25:35', '2020-02-12 09:25:35'),
+('b14946fa8f809b79a45e16e575c538b33178d826d8f947d109250b6e9f340e80d27a9aed053f3df5', 1, 1, 'Test', '[]', 1, '2019-02-12 03:33:43', '2019-02-12 03:33:43', '2020-02-12 09:33:43');
 
 -- --------------------------------------------------------
 
@@ -185,10 +159,7 @@ CREATE TABLE `oauth_clients` (
 --
 
 INSERT INTO `oauth_clients` (`id`, `user_id`, `name`, `secret`, `redirect`, `personal_access_client`, `password_client`, `revoked`, `created_at`, `updated_at`) VALUES
-(1, NULL, 'Prototype Personal Access Client', 'obqYA1LXthawcmMUP5XYivAuEWfzypXLvF3nC0NU', 'http://localhost', 1, 0, 0, '2019-02-04 09:02:21', '2019-02-04 09:02:21'),
-(2, NULL, 'Prototype Password Grant Client', 'VlS9vcdQOHSSto6n416ZM1qVzZJnBk1XcKSWivsb', 'http://localhost', 0, 1, 0, '2019-02-04 09:02:21', '2019-02-04 09:02:21'),
-(3, NULL, 'Prototype Personal Access Client', '3JHjYTM9HeJwbrjfiRqwAus14IUHGR4oh36f5drx', 'http://localhost', 1, 0, 0, '2019-02-04 22:41:38', '2019-02-04 22:41:38'),
-(4, NULL, 'Prototype Password Grant Client', 'FeGuwPlvgCYUd8rL6e6lnZtzNd8kZTKFitD5eB5e', 'http://localhost', 0, 1, 0, '2019-02-04 22:41:38', '2019-02-04 22:41:38');
+(1, NULL, 'Hello', 'boodfECz5DUkh6VuOmWGKCwmnzbEMdI9olyAX9YC', 'http://localhost', 1, 0, 0, '2019-02-12 03:25:20', '2019-02-12 03:25:20');
 
 -- --------------------------------------------------------
 
@@ -208,8 +179,7 @@ CREATE TABLE `oauth_personal_access_clients` (
 --
 
 INSERT INTO `oauth_personal_access_clients` (`id`, `client_id`, `created_at`, `updated_at`) VALUES
-(1, 1, '2019-02-04 09:02:21', '2019-02-04 09:02:21'),
-(2, 3, '2019-02-04 22:41:38', '2019-02-04 22:41:38');
+(1, 1, '2019-02-12 03:25:20', '2019-02-12 03:25:20');
 
 -- --------------------------------------------------------
 
@@ -250,16 +220,6 @@ CREATE TABLE `permissions` (
   `description` text COLLATE utf8mb4_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `permissions`
---
-
-INSERT INTO `permissions` (`id`, `name`, `slug`, `menu`, `description`) VALUES
-(1, 'Tag Create', 'tag-create', 'tag', 'Tag Create'),
-(2, 'Tag Update', 'tag-update', 'tag', 'Tag Update'),
-(3, 'Tag Delete', 'tag-delete', 'tag', 'Tag Delete'),
-(4, 'Tag Publish', 'tag-publish', 'tag', 'Tag Publish');
-
 -- --------------------------------------------------------
 
 --
@@ -270,27 +230,6 @@ CREATE TABLE `permission_role` (
   `role_id` int(10) UNSIGNED NOT NULL,
   `permission_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `permission_role`
---
-
-INSERT INTO `permission_role` (`role_id`, `permission_id`) VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(1, 4),
-(2, 1),
-(2, 2),
-(2, 3),
-(2, 4),
-(3, 4),
-(4, 1),
-(4, 2),
-(4, 3),
-(5, 1),
-(5, 2),
-(3, 2);
 
 -- --------------------------------------------------------
 
@@ -305,16 +244,26 @@ CREATE TABLE `roles` (
   `description` text COLLATE utf8mb4_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `roles`
+-- Table structure for table `todos`
 --
 
-INSERT INTO `roles` (`id`, `name`, `slug`, `description`) VALUES
-(1, 'Superadmin', 'superadmin', 'superadmin'),
-(2, 'Administrator', 'administrator', 'Administrator'),
-(3, 'Editor', 'editor', 'Editor'),
-(4, 'Author', 'author', 'author'),
-(5, 'Contributor', 'contributor', 'contributor');
+CREATE TABLE `todos` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `note` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `todos`
+--
+
+INSERT INTO `todos` (`id`, `title`, `note`, `created_at`, `updated_at`) VALUES
+(1, 'Android', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, eius? Error, assumenda itaque? Nam quas tenetur eaque, voluptatum obcaecati amet provident qui eveniet nisi excepturi et atque possimus magnam nemo impedit exercitationem hic reiciendis aspernatur sequi. Est, vero earum? Minus in facilis nobis recusandae maxime eos sed sint dolore commodi?', '2019-02-11 18:00:00', '2019-02-11 18:00:00');
 
 -- --------------------------------------------------------
 
@@ -342,7 +291,7 @@ CREATE TABLE `users` (
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `account_type` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT 'general' COMMENT 'general/special/employer',
+  `account_type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'general' COMMENT 'general/special/employer',
   `activation_token` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0 or 1',
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -355,9 +304,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `phone`, `email`, `email_verified_at`, `password`, `account_type`, `activation_token`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Jayanta Biswas', '01775219457', 'bjayanta.neo@gmail.com', NULL, '$2y$10$/z7b8pAxERc9Jg0MD506xu4qWXW3tvzgt9FAjZ5k6SE3mOardojnC', 'general', NULL, 1, '1jREhfokg0T0i634f7RYvc8JPbSq4TZUIBXE53F6i8tUsaXaZcTh3y5cZoXo', '2018-12-11 21:36:17', '2018-12-11 21:36:34'),
-(2, 'Robin Biswas', '01792017966', 'brobin.neo@gmail.com', NULL, '$2y$10$OkG3kpyZohg9tox4oiHAIu4f7egpQ0PtqvtND6Sf3A26x.gJSt/dO', 'general', NULL, 1, 'LuB2kOYrG5AvAypjVHxKIcZ1AnOVi9RyHBNe4jkhwplQB6djMSa86Xdtice8', '2019-02-04 13:17:33', '2019-02-04 13:17:33'),
-(9, 'Maruf Hasan', '01735189237', 'emarufhasan@gmail.com', NULL, '$2y$10$TvVkDU3WChvQePMF3/3RCe/CXvZNLUgXt1Iqj9EADKAe0AuJpg4Y6', 'general', NULL, 1, 'oW7FexapaiDChKd9gaBvgQWvFdFcR2fQ9sPDGstzu6THrnKCXPDW7Gi0GhaG', '2019-02-05 06:01:10', '2019-02-05 06:01:46');
+(1, 'Jayanta Biswas', '01775219457', 'uis360.jayanta@gmail.com', NULL, '$2y$10$mfdT34fz9dnurOCHa4xrJeqe.NLXFqUzQw6DYTV9LX92oXDmsVSM6', 'general', NULL, 1, '5mJPAuPnz6msetg2Fg5hoARxV3pxTpS4bomJ4bmqD3yJ5EpwaBCNU3gOgRpd', '2019-02-12 01:43:28', '2019-02-12 01:44:06');
 
 --
 -- Indexes for dumped tables
@@ -451,6 +398,12 @@ ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `todos`
+--
+ALTER TABLE `todos`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `usermeta`
 --
 ALTER TABLE `usermeta`
@@ -479,7 +432,7 @@ ALTER TABLE `adminmeta`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -491,25 +444,31 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `oauth_clients`
 --
 ALTER TABLE `oauth_clients`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `oauth_personal_access_clients`
 --
 ALTER TABLE `oauth_personal_access_clients`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `permissions`
 --
 ALTER TABLE `permissions`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `todos`
+--
+ALTER TABLE `todos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `usermeta`
@@ -521,7 +480,7 @@ ALTER TABLE `usermeta`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables

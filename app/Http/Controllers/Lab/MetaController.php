01828c489e4647b444561c3d78d9ea3653d97a45
Jayanta Biswas <bjayanta.me@gmail.com>
2019-02-25 21:30:45 +0600

    change meta controller and add sqlite db

--- app/Http/Controllers/Lab/MetaController.php
+++ app/Http/Controllers/Lab/MetaController.php
@@ -2,23 +2,40 @@
 
 namespace App\Http\Controllers\Lab;
 
+use DB;
+use Carbon\Carbon;
+use App\Models\System\Meta;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Metadata;
-use DB;
 
 class MetaController extends Controller {
 
     public function getMeta() {
-        return DB::connection('mysql_sys')->table('metadata')->get();
+        return DB::connection('sqlite')->table('metas')->get();
     }
 
     public function setMeta() {
-        $meta = new Metadata;
-        $meta->auth = 'Jayanta Biswas';
-        $meta->email = 'bjayanta.neo@gmail.com';
-        $meta->website = 'jayanta.xyz';
-        $meta->save();
+        $data = [
+            [
+                'meta_key' => 'auth',
+                'meta_value' => 'Jayanta Biswas',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'meta_key' => 'email',
+                'meta_value' => 'bjayanta.neo@gmail.com',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'meta_key' => 'website',
+                'meta_value' => 'jayanta.xyz',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ];
 
+        Meta::insert($data);
     }
 }
